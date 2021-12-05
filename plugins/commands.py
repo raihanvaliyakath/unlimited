03342259970de94a4ea71c3e0fa9be555666b7c1
mirshad517 <72601071+mirshad517@users.noreply.github.com>
2021-12-05 13:19:34 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -265,7 +265,7 @@ async def about(client, message):
             [
                 [
                     InlineKeyboardButton(
-                        "SOURCE CODE", url="https://github.com/TroJanzHEX/Unlimited-Filter-Bot")
+                        "🤖 Code 🤖", url="https://github.com/TroJanzHEX/Unlimited-Filter-Bot")
                 ],
                 [
                     InlineKeyboardButton("BACK", callback_data="help_data"),
